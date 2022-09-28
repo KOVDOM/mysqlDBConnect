3d2783a1409822d19486f95a3844c8e1d3ce5bed
kovdo <kovdo@M1-15>
2022-09-28 11:57:01 +0200

    Add project files.

---
+++ mysqlDBConnect/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace mysqlDBConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queries q=new Queries();
+            q.dbSelect();
+
+            int id;
+            Console.WriteLine("Szeretne törölni(I/N)?");
+            string delete=Console.ReadLine();
+            if (delete=="I")
+            {
+                Console.Write("Kérem az azonosított amit törölni szeretne: ");
+                id = int.Parse(Console.ReadLine());
+                q.dbDelete(id);
+            }else if (delete == "N")
+            {
+                Console.Write("Szeretne új rekorodot felvinni?(I/N) ");
+                string insert=Console.ReadLine();
+                if (insert=="I")
+                {
+                    Console.WriteLine("Kérem a felhasználó nevét? ");
+                    string uname=Console.ReadLine();
+                    Console.WriteLine("Kérem a felhasználó email címét? ");
+                    string email=Console.ReadLine();
+                    q.dbInsert(uname,email);
+                }
+            }
+            Console.WriteLine("Szeretne fríssiteni valamin?(I/N)");
+            string update=Console.ReadLine();
+            if (update=="I")
+            {
+                Console.WriteLine("Kérem az id-t: ");
+                int id2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Kérem a felhasználó nevét? ");
+                string uname = Console.ReadLine();
+                Console.WriteLine("Kérem a felhasználó email címét? ");
+                string email = Console.ReadLine();
+                q.dbUpdate(uname, email, id2);
+            }
+            
+        }
+    }
+}
---
+++ mysqlDBConnect/Queries.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysqlDBConnect
+{
+    internal class Queries
+    {
+        public void dbSelect()
+        {
+            Connect c = new Connect();
+            string query = "SELECT `id`,`uname`,`email` FROM `users`;";
+
+            MySqlCommand cmd = new MySqlCommand(query, c.connect);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            do
+            {
+                Console.WriteLine(reader.GetValue(0) + "-" + reader.GetValue(1) + "-" + reader.GetValue(2));
+
+            } while (reader.Read());
+            reader.Close();
+        }
+
+        public void dbDelete(int identity)
+        {
+            Connect c = new Connect();
+            string query = "DELETE FROM `users` WHERE `id`=" + identity + ";";
+            MySqlCommand cmd = new MySqlCommand(query, c.connect);
+            MySqlDataReader delete = cmd.ExecuteReader();
+            //cmd.ExecuteNonQuery();
+            dbSelect();
+        }
+
+        public void dbInsert(string uname, string email)
+        {
+            Connect c = new Connect();
+            string query = "INSERT INTO `users`(`uname`, `email`) VALUES('" + uname + "','" + email + "');";
+            MySqlCommand cmd = new MySqlCommand(query, c.connect);
+            //MySqlDataReader insert = cmd.ExecuteReader();
+            cmd.ExecuteNonQuery();
+            dbSelect();
+        }
+
+        public void dbUpdate(string uname, string email, int identity)
+        {
+            Connect c = new Connect();
+            string query = "UPDATE `users` SET `uname`='"+uname+"',`email`='"+email+"' WHERE `id`="+identity+";";
+            MySqlCommand cmd = new MySqlCommand(query, c.connect);
+            //MySqlDataReader insert = cmd.ExecuteReader();
+            cmd.ExecuteNonQuery();
+            dbSelect();
+        }
+    }
+}
